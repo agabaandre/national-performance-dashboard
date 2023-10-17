fff978315a91e9732175ed15713066b9cc636fed
AGABA ANDREW <agabaandre@gmail.com>
2023-10-17 12:51:41 +0300

    job kpi

--- application/modules/kpi/views/kpi.php
+++ application/modules/kpi/views/kpi.php
@@ -15,85 +15,97 @@
                                 </i>Add KPI
                             </button>
                             <?php echo form_open_multipart(base_url('kpi/updateKpi'), array('id' => 'kpi', 'class' => 'kpi')); ?>
-                                <button type="submit" class="btn btn-success" style="margin-bottom:3px; width:150px;"><i class="fa fa-circle">
-                                    </i>Update KPI
-                                </button>
+                            <button type="submit" class="btn btn-success" style="margin-bottom:3px; width:150px;"><i class="fa fa-circle">
+                                </i>Update KPI
+                            </button>
 
-                                <div class="card-content">
+                            <div class="card-content">
 
-                                    <table id="kpi" class="table table-responsive table-striped table-bordered">
+                                <table id="kpi" class="table table-responsive table-striped table-bordered">
 
 
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>KPI ID</th>
-                                                <th>Subject Area</th>
-                                                <th>Short Name</th>
-                                                <th>Indicator Statement</th>
-                                                <th>Description</th>
-                                                <th>Data Sources</th>
-                                                <th>Numerator</th>
-                                                <th>Denominator</th>
-                                                <th>Frequency</th>
-                                                <th>Target</th>
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>KPI ID</th>
+                                            <th>Subject Area</th>
+                                            <th>Short Name</th>
+                                            <th>Indicator Statement</th>
+                                            <th>Job</th>
+                                            <th>Data Sources</th>
+                                            <th>Numerator</th>
+                                            <th>Denominator</th>
+                                            <th>Frequency</th>
+                                            <th>Target</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $i = 1;
+                                        $elements = Modules::run('Kpi/kpiData');
+                                        foreach ($elements as $element) : ?>
+
+                                            <tr class="table-row tbrow content strow">
+                                                <td><?php echo $i ?></td>
+                                                <td style="width:15%;"><input type="text" class="form-control" name="kpi_id[]" value="<?php echo $element->kpi_id; ?>" style="border:#000 none; width:70%;" readonly></td>
+                                                <td><?php echo  $element->name; ?></td>
+                                                <input type="hidden" name="subject_area[]" value="<?php echo $element->sid; ?>">
+                                                <input type="hidden" name="is_cumulative[]" value="<?php echo $element->is_cumulative; ?>">
+                                                <td style="width:20%;"><textarea name="short_name[]" rows=4 class="form-control" style="border:#000  none; width:90%;"><?php echo $element->short_name; ?></textarea></td>
+                                                <td style="width:20%;"><textarea name="indicator_statement[]" rows=4 class="form-control" style="border:#000  none; width:90%;"><?php echo $element->indicator_statement; ?></textarea></td>
+                                                <td style="width:35%;">
+
+                                                    <select name="officer_role_id" class="form-control codeigniterselect">
+                                                        <?php $elements = $this->db->get('job')->result();
+                                                        foreach ($elements as $element) : ?>
+                                                            <option value="<?php echo $job=$element->job_id ?>" <?php if ($job == $element->job_id) {
+                                                                                                                echo "selected";
+                                                             } ?>><?php echo $element->job ?></option>
+                                                        <?php endforeach; ?>
+
+                                                    </select>
+
+                                                </td>
+                                                <td style="width:15%;"><textarea name="data_sources[]" rows=4 class="form-control" style="border:#000  none; width:80%;"><?php echo $element->data_sources; ?></textarea></td>
+                                                <td style="width:25%;"><textarea name="numerator[]" rows=5 class="form-control" style="border:#000  none; width:82%;"><?php echo $element->numerator; ?></textarea></td>
+                                                <td style="width:25%;"><textarea name="denominator[]" rows=5 class="form-control" style="border:#000  none; width:82%;"><?php echo $element->denominator; ?></textarea></td>
+
+                                                <td>
+                                                    <select name="frequency[]" class="form-control codeigniterselect">
+                                                        <?php $periods = array("Quarterly", "Monthly", "Weekly", "Annualy");
+
+
+                                                        foreach ($periods as $period) :
+                                                        ?>
+                                                            <option value="<?php echo $period; ?>" <?php if ($period == $element->frequency) {
+                                                                                                        echo "selected";
+                                                                                                    } ?>><?php echo $period; ?></option>
+                                                        <?php endforeach; ?>
+                                                    </select>
+                                                </td>
+                                                <td style="width:10%;"><input type="text" class="form-control" name="current_target[]" value="<?php echo $element->current_target; ?>" style="border:#000 none; width:70%;"></td>
                                             </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            $i = 1;
-                                            $elements = Modules::run('Kpi/kpiData');
-                                            foreach ($elements as $element) : ?>
+                                        <?php
+                                            $i++;
+                                        endforeach;
 
-                                                <tr class="table-row tbrow content strow">
-                                                    <td><?php echo $i ?></td>
-                                                    <td style="width:15%;"><input type="text" class="form-control" name="kpi_id[]" value="<?php echo $element->kpi_id; ?>" style="border:#000 none; width:70%;" readonly></td>
-                                                    <td><?php echo  $element->name; ?></td>
-                                                    <input type="hidden" name="subject_area[]" value="<?php echo $element->sid; ?>">
-                                                    <input type="hidden" name="is_cumulative[]" value="<?php echo $element->is_cumulative; ?>">
-                                                    <td style="width:20%;"><textarea name="short_name[]" rows=4 class="form-control" style="border:#000  none; width:90%;"><?php echo $element->short_name; ?></textarea></td>
-                                                    <td style="width:20%;"><textarea name="indicator_statement[]" rows=4 class="form-control" style="border:#000  none; width:90%;"><?php echo $element->indicator_statement; ?></textarea></td>
-                                                    <td style="width:35%;"><textarea name="description[]" rows=4 class="form-control" style="border:#000  none; width:95%;"><?php echo $element->description; ?></textarea></td>
-                                                    <td style="width:15%;"><textarea name="data_sources[]" rows=4 class="form-control" style="border:#000  none; width:80%;"><?php echo $element->data_sources; ?></textarea></td>
-                                                    <td style="width:25%;"><textarea name="numerator[]" rows=5 class="form-control" style="border:#000  none; width:82%;"><?php echo $element->numerator; ?></textarea></td>
-                                                    <td style="width:25%;"><textarea name="denominator[]" rows=5 class="form-control" style="border:#000  none; width:82%;"><?php echo $element->denominator; ?></textarea></td>
+                                        if (count($elements) == 0) {
 
-                                                    <td>
-                                                        <select name="frequency[]" class="form-control codeigniterselect">
-                                                            <?php $periods = array("Quarterly", "Monthly", "Weekly", "Annualy");
+                                            echo "<tr><td colspan='8'><center><h3 class='text-warning'>Please Add Indicators</h3></center></td></tr>";
+                                        }
+                                        ?>
+                                        </tr>
 
-
-                                                            foreach ($periods as $period) :
-                                                            ?>
-                                                                <option value="<?php echo $period; ?>" <?php if ($period == $element->frequency) {
-                                                                                                            echo "selected";
-                                                                                                        } ?>><?php echo $period; ?></option>
-                                                            <?php endforeach; ?>
-                                                        </select>
-                                                    </td>
-                                                    <td style="width:10%;"><input type="text" class="form-control" name="current_target[]" value="<?php echo $element->current_target; ?>" style="border:#000 none; width:70%;"></td>
-                                                </tr>
-                                            <?php
-                                                $i++;
-                                            endforeach;
-
-                                            if (count($elements) == 0) {
-
-                                                echo "<tr><td colspan='8'><center><h3 class='text-warning'>Please Add Indicators</h3></center></td></tr>";
-                                            }
-                                            ?>
-                                            </tr>
-
-                                        </tbody>
-                            </form>
-                            </table>
+                                    </tbody>
+                                    </form>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </div>
 
 <?php $this->load->view('add_kpi'); ?>
