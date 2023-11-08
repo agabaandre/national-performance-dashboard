a9bc5d12ed57530946335945c1b7609f0d5279b5
Agaba Andrew <agabaandre@gmail.com>
2023-11-08 21:57:32 +0300

    ff

--- application/modules/data/views/subject_charts.php
+++ application/modules/data/views/subject_charts.php
@@ -1,5 +1,18 @@
-
-<div class="row col-md-12">
+<div class="content">
+  <div class="row">
+    <div class="col-sm-12 col-md-12">
+      <div class="panel panel-bd lobidrag">
+        <div class="panel-heading">
+          <div class="panel-title">
+            <h2>Performance</h2>
+          </div>
+        </div>
+        <div class="col-md-12 text-align-center">
+          <h4>Financial Year:
+            <?php echo $this->session->userdata('financial_year'); ?>
+          </h4>
+  </div>
+<div class="panel-body">
 <?php 
 $sub = $this->uri->segment(3);
 $subn = $this->uri->segment(4);
@@ -129,6 +142,15 @@ foreach ($subdash as $subd)
       </tr>
     </tfoot>
     </table>
+
+       </div>
+                    </div>
+                    <div class="panel-footer">
+                     
+                    </div>
+                </div>
+    </div> 
+  </div>
 
 <?php
 
