851e41358f28c4bed1c5872f3c1a97bf2ff0a6f7
Agaba Andrew <agabaandre@gmail.com>
2023-12-01 15:21:56 +0300

    mapped jobs

--- application/modules/person/views/manage_people.php
+++ application/modules/person/views/manage_people.php
@@ -1,119 +1,102 @@
-
 <div class="subheader">
     <h1 class="subheader-title">
- 
+
     </h1>
 </div>
-<div class="card mb-g p-2">
-    <div class="card-body">
-        <div class="alert alert-primary alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">
-                    <i class="fal fa-times"></i>
-                </span>
-            </button>
-            <div class="d-flex flex-start w-100">
-     
-                <div class="d-flex flex-fill">
-                    <div class="flex-fill">
-                   
-                  <div class="col-md-12">
-                                   
-                                    <?php echo form_open_multipart(base_url('person/manage_people'), array('id' => 'person', 'class' => 'person')); ?>
-                                    <div class="row">
-                                        <div class="form-group col-md-6">
-                                            <label for="NAME">Name:</label>
-                                           <input type="text" class="form-control" name="name" placeholder="Health Worker Name" <?php echo $this->input->post('name');?> required> 
-                                        </div>                                     
-                                 
-                                         <div class="form-group col-md-6">
-                                            <label for="Facility">Facility:</label>
-                                            <select class="form-control select2" name="facility">
-                                                <option value=""> SELECT Facility</option>                                             >
-                                                <?php
-                                                foreach ($facilities as $facility) {?>
-                                               <option value="<?php echo $facility->facility_id; ?>" <?php if($this->input->post('facility')==$facility->facility_id){ echo "selected"; }?>><?php echo $facility->facility ?></option>
-                                                <?php } ?>
-                                            </select>
-                                        </div>
 
+<div class="col-md-12">
 
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-
-                                    </div>
-
-                                    <table class="table table-responsive">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Name</th>
-                                                <th>Job</th>
-                                                <th>Facility</th>
-                                                <th>Email</th>
-                                                <th>Telephone</th>
-                                                <th>Select</th>
-
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            $i = 1;
-                                            foreach ($employees as $employee):
-
-                                                ?>
-                                                <tr>
-                                                    <td>
-                                                        <?= $i++ ?>
-
-                                                    </td>
-
-                                                    <td>
-                                                        <?= $employee->surname.' '. $employee->firstname. ' '. $employee->othername ?>
-
-                                                    </td>
-                                                    
-                                                    <td>
-                                                        <?= $employee->job ?>
-
-                                                    </td>
-                                                    
-                                                    <td>
-                                                        <?= $employee->facility ?>
-
-                                                    </td>
-
-                                                    
-                                                    <td>
-                                                        <?= $employee->email ?>
-
-                                                    </td>
-                                                    
-                                                    <td>
-                                                        <?= $employee->telephone ?>
-
-                                                    </td>
-                                                      <td>
-                                                        <a href="<?php echo base_url()?>person/evaluation/<?php echo $employee->ihris_pid; ?>">Confirm</a>
-                                                    
-                                                    </td>
-                                                   
-
-
-                                                    </tr>
-                                            <?php endforeach; ?>
-                                        </tbody>
-                                    </table>
-
-                                    </form>
-
-                                </div>
-
-
-
-                    </div>
-                </div>
-            </div>
+    <?php echo form_open_multipart(base_url('person/manage_people'), array('id' => 'person', 'class' => 'person')); ?>
+    <div class="row">
+        <div class="form-group col-md-6">
+            <label for="NAME">Name:</label>
+            <input type="text" class="form-control" name="name" placeholder="Health Worker Name" <?php echo $this->input->post('name'); ?> required>
         </div>
-       
+
+        <div class="form-group col-md-6">
+            <label for="Facility">Facility:</label>
+            <select class="form-control select2" name="facility">
+                <option value=""> SELECT Facility</option> >
+                <?php
+                foreach ($facilities as $facility) { ?>
+                    <option value="<?php echo $facility->facility_id; ?>" <?php if ($this->input->post('facility') == $facility->facility_id) {
+                           echo "selected";
+                       } ?>>
+                        <?php echo $facility->facility ?>
+                    </option>
+                <?php } ?>
+            </select>
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
     </div>
+
+    <table class="table table-responsive">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Facility</th>
+                <th>Email</th>
+                <th>Telephone</th>
+                <th>Select</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $i = 1;
+            foreach ($employees as $employee):
+
+                ?>
+                <tr>
+                    <td>
+                        <?= $i++ ?>
+
+                    </td>
+
+                    <td>
+                        <?= $employee->surname . ' ' . $employee->firstname . ' ' . $employee->othername ?>
+
+                    </td>
+
+                    <td>
+                        <?= $employee->job ?>
+
+                    </td>
+
+                    <td>
+                        <?= $employee->facility ?>
+
+                    </td>
+
+
+                    <td>
+                        <?= $employee->email ?>
+
+                    </td>
+
+                    <td>
+                        <?= $employee->telephone ?>
+
+                    </td>
+                    <td>
+                        <a href="<?php echo base_url() ?>person/evaluation/<?php echo $employee->ihris_pid; ?>">Confirm</a>
+
+                    </td>
+
+
+
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    </form>
+
+</div>
+
 </div>
