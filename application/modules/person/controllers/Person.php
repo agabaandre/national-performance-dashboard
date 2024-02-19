d76388142aa3f0c88ca271f33c0d02a8b9008c7a
Agaba Andrew <agabaandre@gmail.com>
2024-02-19 16:42:50 +0300

    new stuff

--- application/modules/person/controllers/Person.php
+++ application/modules/person/controllers/Person.php
@@ -729,7 +729,7 @@ function jobs()
 
             $change_password = $this->input->get('changepassword');
 
-          //  dd($data);
+            dd($data);
 
             $this->db->where("ihris_pid", "$ihris_pid");
             $query1 = $this->db->update("ihrisdata", $data);
