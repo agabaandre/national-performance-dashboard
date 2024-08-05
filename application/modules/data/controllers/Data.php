0f569e1e1abd5258f3a8782b9c69f88d4108476f
Agaba Andrew <agabaandre@gmail.com>
2024-08-05 15:15:40 +0300

    fhir data->person performance datatype to string

--- application/modules/data/controllers/Data.php
+++ application/modules/data/controllers/Data.php
@@ -374,7 +374,7 @@ class Data extends MX_Controller {
 
 
 
-	public function get_ihris5data($districts)
+	public function get_ihris5data($disti)
 	{
 		$http = new HttpUtils();
 		$headers = [
@@ -386,7 +386,7 @@ class Data extends MX_Controller {
 		foreach ($districts as $district) {
 
 			//s $dist = str_replace(" District","",$district->name);
-			$dist = $districts;
+			$dist = $disti;
 			$response = $http->sendiHRIS5Request('ihrisdata/' . $dist, "GET", $headers, []);
 
 			if ($response) {
