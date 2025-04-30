0ed5bd0db797a71701679ddeadf5a898eaffc3e4
Agaba Andre <agabaandre@gmail.com>
2025-04-30 20:58:50 +0300

    staff

--- application/modules/kpi/views/kpi.php
+++ application/modules/kpi/views/kpi.php
@@ -302,7 +302,7 @@
         </button>
       </div>
       <div class="modal-body">
-        Are you sure you want to delete this KPI? This action cannot be undone.
+        Are you sure? This action destroys a KPI and its data Permanently.
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
